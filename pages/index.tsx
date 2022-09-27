7b51a9e579f591db9d2f2f24d0b50733a1a9188b
saadbitar1998 <saadbitar458@gmail.com>
2022-09-27 22:04:23 +0300

    Header Implementation

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,8 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Header from "../components/Header";
+import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
   return (
@@ -10,9 +11,21 @@ const Home: NextPage = () => {
         <title>Saad Portfolio</title>
       </Head>
 
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Header />
+
+      {/* Hero */}
+
+      {/* About */}
+
+      {/* Experience */}
+
+      {/* Skills */}
+
+      {/* Projects */}
+
+      {/* Contact Me */}
     </div>
   );
-}
+};
 
 export default Home
