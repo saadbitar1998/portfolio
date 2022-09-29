38974ec7d084702b12b912c89bda96bc6a25d0f5
saadbitar1998 <saadbitar458@gmail.com>
2022-09-29 22:50:01 +0300

    hEro + About Sections

---
+++ components/About.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { motion } from "framer-motion";
+type Props = {};
+
+const About = (props: Props) => {
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 1.5 }}
+      className="flex flex-col relative h-screen text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center"
+    >
+      <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
+        About
+      </h3>
+      <div className="space-y-10 px-0 md:px-10">
+        <h4 className="text-4xl font-semibold">
+          Here is a{" "}
+          <span className="underline decoration-[#F7AB0A]/50">little</span>{" "}
+          background
+        </h4>
+        <p className="text-base ">
+          My name is Saad Bitar! I am a full stack developer. Here is a little
+          bit about me... I have a bachelor degree in Computer Engineering from
+          Beirut Arab University in Lebanon, I have a CGPA of 3.54. I have
+          worked with corporations to help build amazing mobile and web apps
+          both frontend and backend. In addition, I spend my free time learning
+          and keeping up with new technologies, techniques and languages in the
+          tech wold
+        </p>
+      </div>
+    </motion.div>
+  );
+};
+
+export default About;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,27 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import About from "../components/About";
 import Header from "../components/Header";
+import Hero from "../components/Hero";
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
+    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0">
       <Head>
         <title>Saad Portfolio</title>
       </Head>
 
       <Header />
 
-      {/* Hero */}
+      <section id="hero" className="snap-start">
+        <Hero />
+      </section>
 
-      {/* About */}
+      <section id="about" className="snap-center">
+        <About />
+      </section>
 
       {/* Experience */}
 
